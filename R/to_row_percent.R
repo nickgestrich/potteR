9010cd7117bfcf0005d8354c86b1a281d430b8fd
nickgestrich <gestrich@em.uni-frankfurt.de>
2023-07-05 13:13:18 +0200

    to_row_percent passing without warnings

--- R/to_row_percent.R
+++ R/to_row_percent.R
@@ -1,9 +1,24 @@
+#' Turn count data into percentages by row
+#'
+#' @param data a dataframe or tibble of count data
+#'
+#' @return a tibble of percentage data
+#' @importFrom tidyselect where
+#' @importFrom rlang .data
+#' @importFrom tibble tibble
+#' @export
+#'
+#' @examples
+#' x <- tibble::tibble(x = c("a", "b", "c"), y = 4:6, z = 6:8)
+#' to_row_percent(x)
+#' x |> to_row_percent()
+
 to_row_percent <- function(data){
   data |>
     dplyr::rowwise() |> # make calculations rowwise
     dplyr::mutate(sm = sum(dplyr::c_across(where(is.numeric)))) |> #make row sums...
     dplyr::mutate(
       dplyr::across(tidyselect::where(is.numeric),
-                    function(x) (x/sm) * 100)) |>  #...apply in function...
-    dplyr::select(-sm)       # ...then delete
+                    function(x) (x/.data$sm) * 100)) |>  #...apply in function...
+    dplyr::select(-.data$sm)       # ...then delete
 }
---
+++ tests/testthat/test-to_row_percent.R
@@ -0,0 +1,8 @@
+test_that("to_row_percent rows add up to 100", {
+  df <- tibble::tibble(x = 3:5, y = 4:6, z = 6:8)
+  expect_equal(
+    df |>
+      to_row_percent() |>
+      rowSums()
+    , c(100, 100, 100))
+})
