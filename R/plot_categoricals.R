a3aaf140b6a4135b92189bcb85e51dc34bc6011f
nickgestrich <gestrich@uni-frankfurt.de>
2023-07-18 16:13:23 +0200

    print_summary, generate pdf report

---
+++ R/plot_categoricals.R
@@ -0,0 +1,27 @@
+#' Make a barplot of categorical count data in a list
+#'
+#' @param data A list of count data, as created by potteR::sum_categoricals().
+#'
+#' @return A list of barplots.
+#' @importFrom rlang .data
+#' @importFrom stats reorder
+#' @export
+#'
+#'
+#' @examples
+#' guinea[[1]] |>
+#' sum_categoricals() |>
+#' plot_categoricals()
+plot_categoricals <- function(data){
+  out <- vector("list", length= length(data))
+  for(i in seq_along(data)){
+    out[[i]] <- data[[i]] |>
+      dplyr::rename(group = 1) |>
+      ggplot2::ggplot(ggplot2::aes(x = stats::reorder(.data$group,-.data$n), y = .data$n))+
+      ggplot2::geom_bar(stat = "identity")+
+      ggplot2::theme_bw()+
+      ggplot2::labs(title = paste(names(data)[i]),
+                    x = "")
+  }
+  out
+}
---
+++ R/print_summary.R
@@ -0,0 +1,47 @@
+#' Generate a pdf report on all the variables in a dataset
+#'
+#' @param data A dataframe or tibble.
+#' @param file A filepath to a .pdf document
+#'
+#' @return A pdf document.
+#' @importFrom here here
+#' @importFrom gridExtra tableGrob
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' guinea[[3]] |>  print_summary(file = here::here("summary.pdf"))
+#' }
+print_summary <- function(data, file){
+
+  cat("---
+title: \"Summary of variables in `r paste(data)`\"
+date: '`r strftime(Sys.time())`'
+output:
+  pdf_document
+---
+
+\`\`\`{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE, cache = TRUE)
+\`\`\`
+
+
+\`\`\`{r example, results='asis'}
+
+library(patchwork)
+
+l <- plot_summary(data)
+x <- potteR::get_summary(data)
+
+out <- vector('list', length = length(l))
+
+for(i in seq_along(l)) {
+out[[i]] <-
+l[[i]] + gridExtra::tableGrob(x[[i]] |> dplyr::slice_head(n = 15), rows = NULL)
+}
+
+out
+\`\`\`",
+file = "tmp.Rmd")
+  rmarkdown::render("tmp.Rmd", output_file = file)
+}
