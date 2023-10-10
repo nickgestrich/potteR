e844e10df798397fb19eaa85aac9e3b859af9d4f
User-Account <jmpuertaschardt@outlook.de>
2023-10-10 15:07:14 +0200

    adding functions to R and start cleaning vignettes

---
+++ R/add_simple_rim.R
@@ -0,0 +1,28 @@
+#' Remove Numbers from a Column in a Data Frame
+#'
+#' This function takes a data frame and the name of a column as input. It removes numbers from the specified column using regular expressions and returns the modified data frame.
+#'
+#' @param data A data frame containing the column to be modified.
+#' @param column_name The name of the column from which numbers should be removed.
+#'
+#' @return A data frame with numbers removed from the specified column.
+#'
+#' @importFrom dplyr mutate
+#'
+#' @examples
+#' \dontrun{
+#' data <- data.frame(Name = c("John123", "Alice456", "Bob789"))
+#' modified_data <- remove_numbers(data, Name)
+#' }
+#'
+#' @export
+add_simple_rim <- function(data, column_name) {
+  # Ensure the column_name is evaluated as a symbol
+  column_name <- ggplot2::ensym(column_name)
+
+  # Remove numbers from the specified column using regular expressions
+  data <- data |>
+    dplyr::mutate(simple_rim_type = gsub("([0-9].*)", "", !!column_name))
+
+  return(data)
+}
